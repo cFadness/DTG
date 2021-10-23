09f5cce2f455310e4c18382ae348b636816d32f0
cFadness <codie1989@gmail.com>
2021-10-23 15:43:07 -0700

    Deleted the old 'commented out' code. Adjusted prompts to make them look nicer. Tested entire program multiple times and confirmed it works properly. Program complete.

--- Project.js
+++ Project.js
@@ -1,205 +1,5 @@
 "use strict"
 
-// let destinationVariable
-// let randomNumber0to9=Math.floor(Math.random() * 10)
-// function randomDestination(theRandomNumber, destination){            // Tested function. Works properly.
-//     if(theRandomNumber===0){
-//         destination="Houston"
-//         return destination
-//     }
-//     else if(theRandomNumber===1){
-//         destination="Chicago"
-//         return destination
-//     }
-//     else if(theRandomNumber===2){
-//         destination="Duluth"
-//         return destination
-//     }
-//     else if(theRandomNumber===3){
-//         destination="San Diego"
-//         return destination
-//     }
-//     else if(theRandomNumber===4){
-//         destination="Salem"
-//         return destination
-//     }
-//     else if(theRandomNumber===5){
-//         destination="Miami"
-//         return destination
-//     }
-//     else if(theRandomNumber===6){
-//         destination="Phoenix"
-//         return destination
-//     }
-//     else if(theRandomNumber===7){
-//         destination="Cleveland"
-//         return destination
-//     }
-//     else if(theRandomNumber===8){
-//         destination="New York City"
-//         return destination
-//     }
-//     else{
-//         destination="Jacksonville"
-//         return destination
-//     }
-// }
-// let randomDestination10=randomDestination(randomNumber0to9, destinationVariable)
-
-// let restaurantVariable
-// let randomNumber0to7=Math.floor(Math.random() * 8)
-// function randomRestaurant(theRandomNumber, restaurant){                
-//     if(theRandomNumber===0){
-//         restaurant="McDonald's"
-//         return restaurant
-//     }
-//     else if(theRandomNumber===1){
-//         restaurant="Wendy's"
-//         return restaurant
-//     }
-//     else if(theRandomNumber===2){
-//         restaurant="Burger King"
-//         return restaurant
-//     }
-//     else if(theRandomNumber===3){
-//         restaurant="Taco Bell"
-//         return restaurant
-//     }
-//     else if(theRandomNumber===4){
-//         restaurant="Subway"
-//         return restaurant
-//     }
-//     else if(theRandomNumber===5){
-//         restaurant="Jack in the Box"
-//         return restaurant
-//     }
-//     else if(theRandomNumber===6){
-//         restaurant="White Castle"
-//         return restaurant
-//     }
-//     else{
-//         restaurant="Buffalo Wild Wings"
-//         return restaurant
-//     }
-// }
-// let randomRestaurant8=randomRestaurant(randomNumber0to7, restaurantVariable)
-
-// let transportationArray=["Rental Car", "Biking", "Uber", "Walking"]
-// let randomNumber0to3=Math.floor(Math.random() * 4)
-// function randomTransportation(array){ 
-//     randomNumber0to3             
-//     if(randomNumber0to3===0){
-//         return array[0]
-//     }
-//     else if(randomNumber0to3===1){
-//         return array[1]
-//     }
-//     else if(randomNumber0to3===2){
-//         return array[2]
-//     }
-//     else{
-//         return array[3]
-//     }
-// }
-// let randomTransportation4=randomTransportation(transportationArray)
-
-// let entertainmentVariable
-// let randomNumber0to4=Math.floor(Math.random() * 5)
-// function randomEntertainment(entertainment){ 
-//     randomNumber0to4          
-//     if(randomNumber0to4===0){
-//         entertainment="Sight Seeing"
-//         return entertainment
-//     }
-//     else if(randomNumber0to4===1){
-//         entertainment="Bowling"
-//         return entertainment
-//     }
-//     else if(randomNumber0to4===2){
-//         entertainment="Comedy Show"
-//         return entertainment
-//     }
-//     else if(randomNumber0to4===3){
-//         entertainment="Dancing"
-//         return entertainment
-//     }
-//     else{
-//         entertainment="Movie Theatre"
-//         return entertainment
-//     }
-// }
-// let randomEntertainment5=randomEntertainment(entertainmentVariable)
-
-// let tripDetails=`1. Destination: ${randomDestination10}
-// 2. Restaurant: ${randomRestaurant8}
-// 3. Transportation: ${randomTransportation4}
-// 4. Entertainment: ${randomEntertainment5}`
-
-// let firstPrompt=prompt(`Your trip looks like this:
-
-// ${tripDetails}
-
-// Would you like to change it? "Yes" or "No"`)              // First prompt is working and looks good.
-
-// if(firstPrompt==="Yes"){
-//     let newPrompt=prompt(`What would you like to change? Enter 1, 2, 3, or 4
-    
-//     1. Destination: ${randomDestination10}
-//     2. Restaurant: ${randomRestaurant8}
-//     3. Transportation: ${randomTransportation4}
-//     4. Entertainment: ${randomEntertainment5}
-    
-//     Enter "OK" to finish selection`)
-//     while(newPrompt>=1 && newPrompt<=4){
-//         if(newPrompt==1){
-//             newPrompt=prompt(`What would you like to change? Enter 1, 2, 3, or 4
-    
-//     1. Destination: ${randomDestination10}
-//     2. Restaurant: ${restaurantVariable}
-//     3. Transportation: ${transportationVariable}
-//     4. Entertainment: ${entertainmentVariable}
-    
-//     Enter "OK" to finish selection`)
-//         }
-//         else if(newPrompt==2){
-//             newPrompt=prompt(`What would you like to change? Enter 1, 2, 3, or 4
-    
-//     1. Destination: ${destinationVariable}
-//     2. Restaurant: ${randomRestaurant8}
-//     3. Transportation: ${transportationVariable}
-//     4. Entertainment: ${entertainmentVariable}
-    
-//     Enter "OK" to finish selection`)
-//         }
-//         else if(newPrompt==3){
-//             newPrompt=prompt(`What would you like to change? Enter 1, 2, 3, or 4
-    
-//     1. Destination: ${destinationVariable}
-//     2. Restaurant: ${restaurantVariable}
-//     3. Transportation: ${randomTransportation4}
-//     4. Entertainment: ${entertainmentVariable}
-    
-//     Enter "OK" to finish selection`)
-//         }
-//         else if(newPrompt==4){
-//             newPrompt=prompt(`What would you like to change? Enter 1, 2, 3, or 4
-    
-//     1. Destination: 
-//     2. Restaurant: 
-//     3. Transportation:
-//     4. Entertainment: ${randomEntertainment5}
-    
-//     Enter "OK" to finish selection`)
-//         }
-//         else{}
-//     }
-//     if(newPrompt==="OK"){
-//         prompt("Your trip has been finalized. Have fun!")
-        
-//     }
-// }
-
-
 let firstPrompt
 let secondPrompt
 
@@ -255,9 +55,12 @@ function randomEntertainmentSelector(arrayOne,arrayTwo){
     }
 }
 
-firstPrompt=prompt(`This is the Random Trip Generator.
+firstPrompt=prompt(`This is the Random Day Trip Generator.
 
+${randomDestinationSelector(destinationArray, destinationPlaceHolder)}
 ${randomRestaurantSelector(restaurantArray, restaurantPlaceHolder)}
+${randomTransportationSelector(transportationArray, transportationPlaceHolder)}
+${randomEntertainmentSelector(entertainmentArray, entertainmentPlaceHolder)}
 
 Type 'Next' to continue`)
 
@@ -275,22 +78,21 @@ firstPrompt="Anything, but 'Next'"
 while(secondPrompt>=1 && secondPrompt<=4){
     secondPrompt=prompt(`Your trip looks like this:
 
-    1. Destination: ${randomDestinationSelector(destinationArray, destinationPlaceHolder)}
-    2. Restaurant: ${randomRestaurantSelector(restaurantArray, restaurantPlaceHolder)}
-    3. Transportation: ${randomTransportationSelector(transportationArray, transportationPlaceHolder)}
-    4. Entertainment: ${randomEntertainmentSelector(entertainmentArray, entertainmentPlaceHolder)}
+1. Destination: ${randomDestinationSelector(destinationArray, destinationPlaceHolder)}
+2. Restaurant: ${randomRestaurantSelector(restaurantArray, restaurantPlaceHolder)}
+3. Transportation: ${randomTransportationSelector(transportationArray, transportationPlaceHolder)}
+4. Entertainment: ${randomEntertainmentSelector(entertainmentArray, entertainmentPlaceHolder)}
 
-    Enter 1,2,3, or 4 to make changes. When satisfied, enter OK`)
+    Enter 1, 2, 3, or 4 to make changes. When satisfied, enter OK`)
 }
 
 if(secondPrompt==="OK"){
-    prompt("Trip confirmed! Press OK and view your details in the console.")
+    alert("Trip confirmed! Press OK and view your details in the console.")
     console.log(`Destination: ${randomDestinationSelector(destinationArray, destinationPlaceHolder)}`)
     console.log(`Restaurant: ${randomRestaurantSelector(restaurantArray, restaurantPlaceHolder)}`)
     console.log(`Transportation: ${randomTransportationSelector(transportationArray, transportationPlaceHolder)}`)
     console.log(`Entertainment: ${randomEntertainmentSelector(entertainmentArray, entertainmentPlaceHolder)}`)
 }
 else{
-    prompt("Error! Invalid syntax")
+    alert("Error! Invalid syntax")
 }
-
